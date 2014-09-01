ed27193860b836690009569d07431a14011727ee
Remo Laubacher <remo.laubacher@gmail.com>
2014-09-01 23:15:47 +0200

    first working version

--- src/Controller.php
+++ src/Controller.php
@@ -9,10 +9,13 @@ class Controller
         return 'nothing here';
     }
     
-    public static function displayFile($request)
+    public static function displayFile($request, $response, $service)
     {
         $client = new \Github\Client();
         $fileContent = $client->api('repo')->contents()->download($request->user, $request->repo, $request->file);
-        return $fileContent;
+
+        $uniqId = 'githubist' . uniqid();
+        
+        return $service->render(__DIR__ . '/views/file.phtml', array('fileName' => basename($request->file), 'fileContent' => $fileContent, 'uniqId' => $uniqId));
     }
 }
